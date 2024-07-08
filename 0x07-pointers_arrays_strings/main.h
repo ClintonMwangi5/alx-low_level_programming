8f30a5160b6164b259fc349dcb944ce92698f2f5
killswitch775 <clinton5mwangi@gmail.com>
2024-07-08 16:19:08 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,9 @@
+/* File: main.h */
 #ifndef MAIN_H
 #define MAIN_H
 
-char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+void simple_print_buffer(char *buffer, unsigned int size);
 
 #endif /* MAIN_H */
 
