902edec4ee4454085b88d2bfcc5f3062fcdddbec
killswitch775 <150666968+killswitch775@users.noreply.github.com>
2024-07-09 03:35:00 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,19 @@
+/*
+ * File: 4-puts.c
+ * Auth: Solomon kassa
+ */
+
 #include <stdio.h>
 
 /**
- * main - prints the size of various types
- *Return: 0 if exited properly, non-zero otherwise
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
 }
