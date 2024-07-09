11a2885eefec6faf8cd11b654a57e01b4498a6b3
killswitch775 <150666968+killswitch775@users.noreply.github.com>
2024-07-09 03:33:41 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,19 @@
+/*
+ * File: 5-printf.c
+ * Auth: Solomon kassa
+ */
+
 #include <stdio.h>
 
-
-
 /**
- *main - prints the size of various types
- *Return: 0 if exited properly, non-zero otherwise
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," followed by a new line.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
 }
